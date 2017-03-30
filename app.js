7c8f1a14fea686920f401f662c2ad208718120f6
Joe Shepherd <joeshepmedia@gmail.com>
2017-03-30 10:31:06 -0500

    Add more views and navbar

--- app.js
+++ app.js
@@ -7,12 +7,34 @@ const app = express();
 // pug configuration
 app.set('view engine', 'pug');
 
+app.locals.company = "🍕 Pizza Shack";
+app.locals.body = {};
+app.locals.body.magic = "Foooooo!";
+
 // Middlewares
 app.use(express.static('public'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/about', (req, res, next) => {
+  res.render('about', {page: 'About'});
+});
+
+app.get('/contact', (req, res, next) => {
+  res.render('contact', {page: 'Contact'});
+});
+
+app.get('/login', (req, res, next) => {
+  res.render('login', {page: 'Login'});
+});
+
+app.use((req, res) => {
+  res.render('404');
+});
+
+// ********end of middlewares *********************
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
